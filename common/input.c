7de13e8c5e3bf66a1f40b67fe9316335cd9cab81
abhayanigam <61977981+abhayanigam@users.noreply.github.com>
2020-10-01 09:33:05 +0530

    Update input.c

--- common/input.c
+++ common/input.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <limits.h>
 #include "./../sort_common.h"
+#define p printf(
+#define s scanf(
+#define f )
+#define m printf("\n")
 
 int read_size()
 {
     int n = 0;
     do
     {
-        printf("Enter size of array:\n");
-        scanf("%d", &n);
+        p"Enter size of array:\n" f;
+        s"%d", &n f;
 
         if (n <= 0 || n >= INT_MAX)
         {
-            printf("Invalid array size, try again?\n");
+            p"Invalid array size, try again?" f;
+            m;
         }
     } while (n <= 0 || n >= INT_MAX);
     return n;
@@ -21,10 +26,10 @@ int read_size()
 
 void read_array(int arr[], int n)
 {
-    printf("Enter array to sort:\n");
+    p"Enter array to sort:\n" f;
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &arr[i]);
+        s"%d", &arr[i] f;
     }
 }
 
@@ -35,5 +40,6 @@ void read_array(int arr[], int n)
 //     read_array(arr, n);
 //     print_array(arr, n);
 //     free(arr);
+//     m;
 //     return 0;
 // }
